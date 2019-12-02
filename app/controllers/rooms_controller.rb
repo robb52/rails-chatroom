3f23591bb163a51e9f20d325a33cfee8ba9bfe33
Rob Brown <brown4homes@yahoo.com>
2019-12-02 11:45:23 -0800

    Created new and edit views

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -36,7 +36,7 @@ class RoomsController < ApplicationController
 
   def load_entities
     @rooms = Room.all 
-    @room = Room.find(params[:id] if params[:id]
+    @room = Room.find(params[:id]) if params[:id]
   end
 
   def permitted_parameters
